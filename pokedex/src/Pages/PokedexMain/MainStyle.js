df9ab9181cd416c24c7dcc112707d5abd1cff91e
Pereira-Araujo <araujo_ir@yahoo.com>
2021-03-19 10:30:50 -0300

    Teste do Estado Global

---
+++ pokedex/src/Pages/PokedexMain/MainStyle.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+display:flex;
+justify-content:space-around;
+flex-wrap:wrap;
+
+`
+export const Card = styled.article`
+background:LightSalmon;
+width:8rem;
+height:10rem;
+margin:4px;
+`
--- pokedex/src/Pages/PokedexMain/PokedexMain.js
+++ pokedex/src/Pages/PokedexMain/PokedexMain.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { goToPokemonDetails, goToPokemonList } from '../../routes/Coordinator';
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { goToPokemonDetails, goToPokemonList } from "../../routes/Coordinator";
+import GlobalStateContext from "../../global/GlobalStateContext";
+import { Container, Card } from "./MainStyle";
 
 const PokedexMain = () => {
+  const { pokemons } = useContext(GlobalStateContext);
+
   const history = useHistory();
 
   return (
     <>
-      <p> AQUI É A POKEDEX </p>
-      <button onClick={() => goToPokemonList(history)}> Voltar para lista dos Pokemons </button>
-      <button onClick={() => goToPokemonDetails(history)}> Ver detalhes do Pokemon </button>
+      <Container>
+        {pokemons.map((creature) => {
+          return (
+            <Card key={creature.name}>
+              {creature.name}
+              <img alt={creature.name} src={creature.sprites.front_default} />
+            </Card>
+          );
+        })}
+      </Container>
+      <>
+        <p> AQUI É A POKEDEX </p>
+        <button onClick={() => goToPokemonList(history)}>
+          {" "}
+          Voltar para lista dos Pokemons{" "}
+        </button>
+        <button onClick={() => goToPokemonDetails(history)}>
+          {" "}
+          Ver detalhes do Pokemon{" "}
+        </button>
+      </>
     </>
   );
 };
