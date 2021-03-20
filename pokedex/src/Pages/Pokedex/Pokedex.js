7b089f5404a4bf9be10c359ba2c2be62f18962dc
ricardo-ramos <ricardoalmeidaramos@gmail.com>
2021-03-20 00:55:05 -0300

    lista de pokemons estilizada

---
+++ pokedex/src/Pages/Pokedex/Pokedex.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { goToPokemonDetails, goToPokemonList } from "../../routes/Coordinator";
+
+const Pokedex = () => {
+  const history = useHistory();
+
+  return (
+    <>
+      <p> AQUI É A POKEDEX </p>
+      <button onClick={() => goToPokemonList(history)}>
+        {" "}
+        Voltar para lista dos Pokemons{" "}
+      </button>
+      <button onClick={() => goToPokemonDetails(history)}>
+        {" "}
+        Ver detalhes do Pokemon{" "}
+      </button>
+    </>
+  );
+};
+
+export default Pokedex;
---
+++ pokedex/src/Pages/PokemonList/Styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ListWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100vw;
+`
+
+export const Card = styled.article`
+  align-items: center;
+  background: lightSalmon;
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  margin: 8px;
+  width: 160px;
+`
