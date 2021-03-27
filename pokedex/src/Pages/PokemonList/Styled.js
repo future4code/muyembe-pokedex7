f97a16f4d87cccf952ee4142c9a303d0e7bd75f6
Pereira-Araujo <araujo_ir@yahoo.com>
2021-03-27 10:33:14 -0300

    teste para atualização

--- pokedex/src/Pages/PokemonList/Styled.js
+++ pokedex/src/Pages/PokemonList/Styled.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+// import Wallpaper from "https://wallpaperaccess.com/full/8621.jpg"
 
 export const ListWrapper = styled.section`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   width: 98vw;
+  /* background-image:url("https://c4.wallpaperflare.com/wallpaper/724/474/485/pokemon-go-logo-minimalism-wallpaper-preview.jpg");
+  background-size:100%;
+  background-repeat:no-repeat; */
 `;
 
